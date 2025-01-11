d86c47fbe0861b6fa9e85845fe3882fef05e4a1b
tasotnik <artem.sotnikov@hslu.ch>
2025-01-11 13:58:48 +0100

    Change to hw15

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'but_chosen': "#497CFF",
+        'start_grad': "#FF1CF7",
+        'end_grad': "#00F0FF",
+      },
+    },
+  },
+  plugins: [],
+}
+
