0fe968b75e41ddc154a11590cea5bdf8cf794121
tasotnik <artem.sotnikov@hslu.ch>
2025-03-06 12:14:00 +0100

    Change to OOP with SLider class and instances

---
+++ Slider.js
@@ -0,0 +1,125 @@
+export class Slider {
+    //variables
+    currentSlide = 0;
+    startX;
+   // isOngoing = false;
+   // slideTime = 1; //in seconds
+   // animation;
+
+    //parameters
+
+    constructor() {
+        // Query DOM elements (when several queries are required)
+        this.imgContainerElem = document.querySelector(".slider");
+        this.allImages = document.querySelectorAll(".image_container");
+        this.firstImageElem = document.querySelector("img");
+
+        // Check if there are images
+        if (this.allImages.length === 0) {
+            throw new Error('No images specified');
+        } else {
+            console.log(this.allImages);
+        }
+        this.slidesCount = this.allImages.length;
+
+
+        this.barsSubscription();
+        this.keyArrowsSubscription();
+        this.wheelSubscription();
+        this.touchSubscriptions();
+
+    }
+
+    // Events
+    //On prev and next bars
+    barsSubscription() {
+        //Move to left with prev bar
+        document.querySelector(".prev").addEventListener("click", this.onLeftClick.bind(this));
+        //Move to right with next bar
+        document.querySelector(".next").addEventListener("click", this.onRightClick.bind(this));
+    }
+
+    //On left and right arrows
+    keyArrowsSubscription() {
+        document.addEventListener("keydown", this.onKeyPress.bind(this));
+    }
+
+    //Support touchpad
+    wheelSubscription() {
+        this.imgContainerElem.addEventListener("wheel", this.onWheelMove.bind(this));
+    }
+
+    //Support mobile swipe
+    touchSubscriptions () {
+        imgContainerElem.addEventListener("touchstart", this.onTouchStart.bind(this));
+        imgContainerElem.addEventListener("touchend", this.onTouchEnd.bind(this));
+    }
+
+
+    //Move to left with prev bar
+    onLeftClick() {
+        this.currentSlide--;
+
+        if(this.currentSlide < 0) {
+            this.currentSlide = this.slidesCount - 1;
+        }
+
+        this.imgContainerElem.style.transform = `translate(-${this.currentSlide * this.firstImageElem.offsetWidth}px)`;
+
+    //    updateActiveBullet();
+    }
+
+    //Move to right with next bar
+    onRightClick() {
+        this.currentSlide++;
+
+        if(this.currentSlide === this.slidesCount) {
+            this.currentSlide = 0;
+        }
+
+        this.imgContainerElem.style.transform = `translate(-${this.currentSlide * this.firstImageElem.offsetWidth}px)`;
+        this.imgContainerElem.style.transform = `translate(-${this.currentSlide * this.firstImageElem.offsetWidth}px)`;
+
+        //updateActiveBullet();
+    }
+
+    //Check left or right key press then activate functionality of respective bars
+    onKeyPress(event) {
+        if (event.key === "ArrowLeft") {
+            this.onLeftClick();
+        } else if (event.key === "ArrowRight") {
+            this.onRightClick();
+        }
+    }
+
+    //Move on touchpad
+    onWheelMove(event) {
+        //Introduce some threshold, otherwise uncontrollable behaviour (too "sensitive"/fast)
+        const thresholdMoveWheel = 20; //px
+
+        if (event.deltaX > thresholdMoveWheel) {
+            //console.log(event.deltaX);
+            this.onRightClick();
+        } else if (event.deltaX < -thresholdMoveWheel) {
+            this.onLeftClick();
+        }
+    }
+
+    //"Log" start position on mobile touch.
+    onTouchStart(event) {
+        this.startX = event.touches[0].clientX;
+    }
+
+    //Swipe on mobile
+    onTouchEnd(event) {
+        let endX = event.changedTouches[0].clientX;
+
+        if (this.startX > endX) {
+            this.onRightClick();
+        } else if (this.startX < endX) {
+            this.onLeftClick();
+        }
+    }
+
+
+}
--- main.js
+++ main.js
@@ -1,118 +1,28 @@
+import {Slider} from "./Slider.js";
+
+const slider = new Slider({ });
+
 // Variables
-let currentSlide = 0;
-let startX;
 let isOngoing = false;
 const slideTime = 2; //in seconds
 let animation;
 
 // Query DOM elements
-const imgContainerElem = document.querySelector(".slider");
-console.log(imgContainerElem);
-const firstImageElem = document.querySelector("img");
-console.log(firstImageElem);
-//Prev and next bars
-const leftElement = document.querySelector(".prev");
-const rightElement = document.querySelector(".next");
-console.log(leftElement);
-console.log(rightElement);
-const allImages = document.querySelectorAll(".image_container");
-console.log(allImages);
 const allBullets = document.querySelectorAll(".bullet");
 console.log(allBullets);
 const startStopElem = document.querySelector(".start_stop");
 console.log(startStopElem);
 
-const slidesCount = allImages.length;
-console.log(slidesCount);
+
 
 // Events
-//On prev and next bars
-leftElement.addEventListener("click", onLeftClick);
-rightElement.addEventListener("click", onRightClick);
-//On left and right arrows
-document.addEventListener("keydown", onKeyPress);
-//Support mobile swipe
-imgContainerElem.addEventListener("touchstart", onTouchStart);
-imgContainerElem.addEventListener("touchend", onTouchEnd);
-//Support touchpad
-imgContainerElem.addEventListener("wheel", onWheelMove);
+
 //Navigation button
 allBullets.forEach(bullet => bullet.addEventListener("click", onBulletClick));
 startStopElem.addEventListener("click", onStartStop);
 
 
 // Listeners
-//Move to left with prev bar
-function onLeftClick() {
-    currentSlide--;
-
-    if(currentSlide < 0) {
-        currentSlide = slidesCount - 1;
-    }
-
-    imgContainerElem.style.transform = `translate(-${currentSlide * firstImageElem.offsetWidth}px)`;
-
-    updateActiveBullet();
-}
-
-//Move to right with next bar
-function onRightClick() {
-    currentSlide++;
-
-    if(currentSlide === slidesCount) {
-        currentSlide = 0;
-    }
-
-    imgContainerElem.style.transform = `translate(-${currentSlide * firstImageElem.offsetWidth}px)`;
-
-    updateActiveBullet();
-}
-
-//Check left or right key press then activate functionality of respective bars
-function onKeyPress(event) {
-   // console.log(event);
-
-    if (event.key === "ArrowLeft") {
-        onLeftClick();
-    } else if (event.key === "ArrowRight") {
-        onRightClick();
-    }
-}
-
-//"Log" start position on mobile touch.
-function onTouchStart(event) {
-    //console.log(event);
-    startX = event.touches[0].clientX;
-    //console.log(startX);
-}
-
-//Swipe on mobile
-function onTouchEnd(event) {
-    //console.log(event);
-    let endX = event.changedTouches[0].clientX;
-    //console.log(endX);
-
-    if (startX > endX) {
-        onRightClick();
-    } else if (startX < endX) {
-        onLeftClick();
-    }
-}
-
-//Move on touchpad
-function onWheelMove(event) {
-    //console.log(event);
-
-    //Introduce some threshold, otherwise uncontrollable behaviour (too "sensitive"/fast)
-    const thresholdMoveWheel = 20; //px
-
-    if (event.deltaX > thresholdMoveWheel) {
-        //console.log(event.deltaX);
-        onRightClick();
-    } else if (event.deltaX < -thresholdMoveWheel) {
-        onLeftClick();
-    }
-}
 
 //Scroll slides with navigation bullets
 function  onBulletClick(event) {
