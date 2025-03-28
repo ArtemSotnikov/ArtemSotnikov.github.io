6834429dbbc0c1b6d612826c5a3cb2ddbc2ea36d
Artem <artemsotnikov@gmail.com>
2025-03-28 12:09:06 +0100

    Change to hw33

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import {MovieSearch} from "./MovieSearch.js";
-
-const movieSearch = new MovieSearch('7377b7dd','https://www.omdbapi.com/', "search_movie");
-
-
-
+const a = 5;
+const b = 5;
+console.log(a + b);
